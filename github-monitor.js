72e890ff6e17d602e8e7577cc0d8b1bfa0232626
Anthony Dillon <me@anthonydillon.com>
2016-11-28 23:03:06 +0000

    Getting there

--- github-monitor.js
+++ github-monitor.js
@@ -1,14 +1,35 @@
+var fetch = require('node-fetch')
+
 let user = 'ubuntudesign'
+let auth = ' -u anthonydillon:Ringw00d'
 
 const gh_api = {
     'repos': `https://api.github.com/users/${user}/repos`,
-    'pulls': 'https://api.github.com/repos/ubuntudesign/{}/pulls'
+    'pulls': `https://api.github.com/repos`
 }
 
-fetch(gh_api.repos, {
-	method: 'get'
-}).then(function(response) {
-  console.log(response.blob());
-}).catch(function(err) {
-	console.error('Whoops, there was an error');
-});
+function processRepos(repos) {
+  console.log('Processing user repos...');
+  for (var i in repos) {
+    let repoName = repos[i].name
+    console.log(repos[i])
+    fetch(`${gh_api.pulls}/${repos[i].full_name}/pulls`)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(res) {
+        console.log(`Repo ${repoName} has ${res.length} pulls`)
+      })
+  }
+}
+
+function fetchRepos() {
+  fetch(gh_api.repos)
+    .then(function(response) {
+      console.log('Fetching user repos...');
+      return response.json()
+    })
+    .then(processRepos)
+}
+
+fetchRepos();
